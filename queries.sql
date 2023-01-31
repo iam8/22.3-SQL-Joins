406b71fecd9aee253d42e791da23ed9204ffb8fa
alexm <alex-m@outlook.com>
2023-01-30 18:50:37 -0800

    Add rounding to average price in HAVING clause; attempt indentation for organization and clarity

--- queries.sql
+++ queries.sql
@@ -6,16 +6,19 @@
 -- an owner_id.
 SELECT *
 FROM owners
-LEFT JOIN vehicles
-ON owners.id = vehicles.owner_id;
+FULL OUTER JOIN vehicles
+    ON owners.id = vehicles.owner_id;
 
 
 -- 2) Count the number of cars for each owner. Display the owners' first_name, last_name and count
 -- of vehicles. The first_name should be ordered in ascending order.
-SELECT first_name, last_name, COUNT(*) AS num_vehicles
+SELECT
+    first_name,
+    last_name,
+    COUNT(*) AS num_vehicles
 FROM owners
 JOIN vehicles
-ON owners.id = vehicles.owner_id
+    ON owners.id = vehicles.owner_id
 GROUP BY owners.id
 ORDER BY first_name ASC;
 
@@ -24,11 +27,15 @@ ORDER BY first_name ASC;
 -- as integers. Display the owners first_name, last_name, average price and count of vehicles. The
 -- first_name should be ordered in descending order. Only display results with more than one
 -- vehicle and an average price greater than 10000.
-SELECT first_name, last_name, ROUND(AVG(price)) AS avg_price, COUNT(*) AS num_vehicles
+SELECT
+    first_name,
+    last_name,
+    ROUND(AVG(price)) AS avg_price,
+    COUNT(*) AS num_vehicles
 FROM owners
 JOIN vehicles
-ON owners.id = vehicles.owner_id
+    ON owners.id = vehicles.owner_id
 GROUP BY owners.id
-HAVING AVG(price) > 10000
-AND COUNT(*) > 1
+HAVING
+    ROUND(AVG(price)) > 10000 AND COUNT(*) > 1
 ORDER BY first_name DESC;
