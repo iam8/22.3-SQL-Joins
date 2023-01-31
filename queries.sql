3733a85bd11ecd72406940f7b9704027d4fc1a66
alexm <alex-m@outlook.com>
2023-01-30 18:20:30 -0800

    Add comments with question prompts; add response for question 1

--- queries.sql
+++ queries.sql
@@ -1,2 +1,21 @@
 -- Ioana A Mititean
 -- 22.3: SQL Joins Exercise
+
+
+-- 1) Join the two tables so that every column and record appears, regardless of if there is not
+-- an owner_id.
+SELECT *
+FROM owners
+LEFT JOIN vehicles
+ON owners.id = vehicles.owner_id;
+
+
+-- 2) Count the number of cars for each owner. Display the owners' first_name, last_name and count
+-- of vehicles. The first_name should be ordered in ascending order.
+
+
+
+-- 3) Count the number of cars for each owner and display the average price for each of the cars
+-- as integers. Display the owners first_name, last_name, average price and count of vehicles. The
+-- first_name should be ordered in descending order. Only display results with more than one
+-- vehicle and an average price greater than 10000.
